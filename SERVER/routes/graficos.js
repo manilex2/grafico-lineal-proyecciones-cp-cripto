b0456aae066e8ea7b8e2a8149a8f4bd3bcb4f376
Manuel Pereira <manilex2@gmail.com>
2022-06-20 08:59:07 -0500

    Actualizado para obtener el grafico de la nueva forma

--- SERVER/routes/graficos.js
+++ SERVER/routes/graficos.js
@@ -4,7 +4,8 @@ const pool = require('../database');
 
 router.get('/grafico/:indice', async (req, res) => {
     const { indice } = req.params;
-    const datos = await pool.query(`SELECT * FROM ${process.env.TABLE_CRIPTO} WHERE cripto = ? ORDER BY fecha ASC`, [indice]);
+    var sql = `SELECT PC.id, PC.cripto, PC.fecha, PC.id_precio, PC.forecast, PC.pesimista, PC.optimista, PC.id_grupo, PA.precio AS precio FROM ${process.env.TABLE_CRIPTO_PROY_CP} AS PC LEFT JOIN ${process.env.TABLE_CRIPTO_PRECIO_ACTUAL} AS PA ON PC.id_precio = PA.id WHERE cripto = ? ORDER BY fecha ASC`
+    const datos = await pool.query(sql, [indice]);
     const datosExtraidos = JSON.stringify(datos);
     res.send(datosExtraidos);
 });
